14c9846faebdca24fe35db8cdba5a0c733fbd3c8
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-06-16 19:39:58 +0200

    Preparing Public

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,40 @@
 import 'normalize.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Αρχική, Εισαγωγική } from './pages';
-import { Έργα } from './components';
+import {
+  Δεξιότητες,
+  Επικοινωνία,
+  Έργα,
+  ΠοιοςΕίμαι,
+  Τεχνολογίες,
+} from './components';
 
 function App() {
   return (
     <>
-      <div className='κατασκευή'>Υπό Κατασκευή!!</div>
+      <div className="κατασκευή">Υπό Κατασκευή!!</div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Αρχική />}>
             <Route
-              path={'τεχνολογίες' || 'technologies' || 'tekniker'}
-              element={<Έργα />}
+              path={'ποιος-είμαι' || 'who-am-i' || 'färdigheter'}
+              element={<ΠοιοςΕίμαι />}
+            />
+            <Route
+              path={'δεξιότητες' || 'skills' || 'färdigheter'}
+              element={<Δεξιότητες />}
+            />
+            <Route
+              path={'επικοινωνία' || 'contact' || 'Kontakt'}
+              element={<Επικοινωνία />}
             />
             <Route
               path={'έργα' || 'projects' || 'projekt'}
               element={<Έργα />}
+            />
+            <Route
+              path={'τεχνολογίες' || 'technologies' || 'tekniker'}
+              element={<Τεχνολογίες />}
             />
           </Route>
           <Route path="/landing" element={<Εισαγωγική />} />
