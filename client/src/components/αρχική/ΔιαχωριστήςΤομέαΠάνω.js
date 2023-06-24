f658fd4afce8e26c41d9ac7c747f8c443302f943
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-06-24 22:43:48 +0200

    Δημιουργία Διαχωριστών

---
+++ client/src/components/αρχική/ΔιαχωριστήςΤομέαΠάνω.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+const ΔιαχωριστήςΤομέαΠάνω = () => {
+  return (
+    <ΠεριέκτηςΔιαχωριστών>
+      <ΔιαχωριστήςΤομέα διαχωριστής αριστερά />
+      <ΔιαχωριστήςΤομέα διαχωριστής κέντρο />
+      <ΔιαχωριστήςΤομέα διαχωριστής δεξιά />
+    </ΠεριέκτηςΔιαχωριστών>
+  );
+};
+
+const ΠεριέκτηςΔιαχωριστών = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 40px;
+`;
+
+const ΔιαχωριστήςΤομέα = styled.div`
+  width: ${(props) => (props.κέντρο ? '250px' : '150px')};
+  height: 6px;
+  border-radius: 10px;
+  background-color: #fff;
+  background: ${(props) =>
+    props.αριστερά
+      ? 'linear-gradient(90deg, #13ADC7 0%, #945DD6 50%, #945DD6 100%)'
+      : (props) =>
+          props.κέντρο
+            ? 'linear-gradient(90deg, #945DD6 0%, #13ADC7 45%, #13ADC7 55%, #945DD6 100%)'
+            : (props) =>
+                props.δεξιά
+                  ? 'linear-gradient(90deg, #945DD6 0%, #945DD6 50%, #13ADC7 100%)'
+                  : 'linear-gradient(90deg, #945DD6 0%, #13ADC7 50%, #945DD6 100%)'};
+  margin: ${(props) => (props.διαχωριστής ? '4rem 0 3rem' : '3rem 0 4rem')};
+`;
+
+export default ΔιαχωριστήςΤομέαΠάνω;
