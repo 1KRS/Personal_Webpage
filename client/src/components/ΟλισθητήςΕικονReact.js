a3cf91870d2c5b753f98b4b4d37de31604c30abc
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2023-07-16 21:50:02 +0200

    Create ΟλισθητήςΕικονReact.js

---
+++ client/src/components/ΟλισθητήςΕικονReact.js
@@ -0,0 +1,20 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
+const ΟλισθητήςΕικονReact = ({ props }) => {
+  return (
+    // <swiper-container>
+    //   <swiper-slide>Slide 1</swiper-slide>
+    //   <swiper-slide>Slide 2</swiper-slide>
+    //   <swiper-slide>Slide 3</swiper-slide>
+    //   <swiper-slide>Slide ...</swiper-slide>
+    // </swiper-container>
+    <swiper-container>
+      <swiper-slide>Slide 1</swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+      <swiper-slide>Slide ...</swiper-slide>
+    </swiper-container>
+  );
+};
+export default ΟλισθητήςΕικονReact;
